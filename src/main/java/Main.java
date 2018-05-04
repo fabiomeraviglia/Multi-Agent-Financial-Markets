34a988d0ea3fbefaa2b0b62748ad0062a48b8cf2
danilob <benetti.danilo@gmail.com>
2018-05-04 23:57:27 +0200

    Built MLNN test model

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,45 @@
-import com.opencsv.CSVReader;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Sgd;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        CSVReader reader = new CSVReader(new FileReader("res/AAPL_BID_RES_1M_RANGE_1Y.csv"), ',');
-        List<String[]> fields = reader.readAll();
-        fields.remove(0);
-        List<Integer> close_prices = new ArrayList<Integer>();
-        for (String[] f : fields) close_prices.add((int) (Float.parseFloat(f[4]) * 1000));
+        List<String> rows = Files.readAllLines(Paths.get("res/AAPL_BID_RES_1M_RANGE_1Y.csv"));
+        List<Integer> close_prices = new ArrayList<>();
+        for (String r : rows) close_prices.add((int) (Float.parseFloat(r.split(",")[4]) * 1000));
+
+        MultiLayerConfiguration nn_conf = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(new Sgd(0.05))
+                .list()
+                .layer(0, new DenseLayer.Builder()
+                        .nIn(60)
+                        .nOut(120)
+                        .activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(1, new DenseLayer.Builder()
+                        .nIn(120)
+                        .nOut(30)
+                        .activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(2, new DenseLayer.Builder()
+                        .nIn(30)
+                        .nOut(5)
+                        .activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .pretrain(false).backprop(true)
+                .build();
     }
 }
