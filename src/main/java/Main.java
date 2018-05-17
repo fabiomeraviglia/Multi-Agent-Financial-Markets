8828969f0e0d1a7329f7f7f94e82247a2a77fa7e
FabMer <fabiomeraviglia@gmail.com>
2018-05-17 18:33:38 +0200

    CIAO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,45 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import com.google.common.collect.ImmutableMap;
+import javafx.util.Pair;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.*;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.GravesLSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Sgd;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import com.google.common.collect.ImmutableMap;
 
+import javafx.util.Pair;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         List<String> rows = Files.readAllLines(Paths.get("res/AAPL_BID_RES_1M_RANGE_1Y.csv"));
         List<Integer> close_prices = new ArrayList<>();
-        for (String r : rows) close_prices.add((int) (Float.parseFloat(r.split(",")[4]) * 1000));
+        for (int i=1; i<rows.size();i++) close_prices.add((int) (Float.parseFloat(rows.get(i).split(",")[4]) * 1000));
 
-        MultiLayerConfiguration nn_conf = new NeuralNetConfiguration.Builder()
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .updater(new Sgd(0.05))
-                .list()
-                .layer(0, new DenseLayer.Builder()
-                        .nIn(60)
-                        .nOut(120)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .layer(1, new DenseLayer.Builder()
-                        .nIn(120)
-                        .nOut(30)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .layer(2, new DenseLayer.Builder()
-                        .nIn(30)
-                        .nOut(5)
-                        .activation(Activation.SOFTMAX)
-                        .weightInit(WeightInit.XAVIER)
-                        .build())
-                .pretrain(false).backprop(true)
-                .build();
+
+
     }
+
 }
